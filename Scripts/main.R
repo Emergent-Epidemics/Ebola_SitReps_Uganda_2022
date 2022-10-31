57c6413b51c9e47fb75f2a7e4fbaadcd3857f07e
Sam <sscarpin@gmail.com>
2022-10-31 12:01:12 -0400

    adding plots

--- Scripts/main.R
+++ Scripts/main.R
@@ -2,13 +2,6 @@
 #Oct 2022
 #Main file for Uganda Ebola line list.
 
-###########
-#libraries#
-###########
-library(ggplot2)
-library(dplyr)
-library(lme4)
-
 #########
 #Globals#
 #########
@@ -25,6 +18,14 @@ source("build_line_list.R")
 #Plot cases#
 ############
 if(do_plot == TRUE){
+  ###########
+  #libraries#
+  ###########
+  library(ggplot2)
+  library(dplyr)
+  library(lme4)
+  library(zoo)
+  
   rm_NA_district <- which(is.na(baselinelist$District) == TRUE)
   baselinelist$count <- 1
   baselinelist$Date_confirmation <- as.Date(baselinelist$Date_confirmation)
@@ -37,13 +38,29 @@ if(do_plot == TRUE){
   
   ggplot(data = baselinelist, aes(x = as.Date(Date_confirmation), fill = District)) + geom_bar() + facet_wrap(~District) + xlab("Daily new Ebola cases") + ylab("Daily new Ebola cases") + theme(legend.position = "none", legend.key = element_rect(fill = "#f0f0f0"), legend.background = element_rect(fill = "#ffffffaa", colour = "black"), panel.background = element_rect(fill = "white", colour = "black"), axis.text.y = element_text(colour = "black", size = 14), axis.text.x = element_text(colour = "black", size = 10), axis.title = element_text(colour = "black", size = 16), panel.grid.minor = element_line(colour = "#00000080",linetype = 3), panel.grid.major = element_line(colour = "#00000000", linetype = 3)) 
   
-  time_reg <- as.numeric(plot_dat$Date_confirmation - min(plot_dat$Date_confirmation, na.rm = TRUE), unit = "days")
-  prov_reg <- plot_dat$District
-  cases_reg <- plot_dat$cumsum
+  time_reg <- as.numeric(plot_dat_district$Date_confirmation - min(plot_dat_district$Date_confirmation, na.rm = TRUE), unit = "days")
+  prov_reg <- plot_dat_district$District
+  cases_reg <- plot_dat_district$cumsum
   mod <- lmer(log(cases_reg+1) ~ time_reg + (time_reg|prov_reg))
   doubling <- data.frame(row.names(ranef(mod)$prov_reg), log(2)/(ranef(mod)$prov_reg$time_reg+fixef(mod)["time_reg"]))
   colnames(doubling) <- c("District", "Doubling Time (days)")
   doubling <- doubling[order(doubling$`Doubling Time (days)`),]
   
   by_date <- by(baselinelist$ID, baselinelist$Date_confirmation, length)
+  by_date_dates <- names(by_date)
+  by_date_dates <- as.POSIXct(strptime(by_date_dates, format = "%Y-%m-%d"))
+  time_reg_country <- as.numeric(by_date_dates - min(by_date_dates, na.rm = TRUE), unit = "days")
+  cases_reg_country <- rollmean(as.numeric(by_date), k = 7)
+  mod2 <- lm(log(cases_reg_country) ~ time_reg_country[-c(1:6)])
+  doubling_country <- log(2)/mod2$coefficients[2]
+  
+  plot(by_date_dates[-c(1:6)], log(cases_reg_country), type = "l", bty = "n", lwd = 3, xlab = "2022", ylab = "Daily new Ebola cases (7 day avg, log-scale)")
+  abline(mod2)
+  
+  rates <- rep(NA, length(cases_reg_country))
+  for(i in 1:(length(cases_reg_country)-3)){
+    mod.i <- lm(log(cases_reg_country[i:(i+3)]) ~ time_reg_country[-c(i:6)][i:(i+3)])
+    rates[i] <- log(2)/mod.i$coefficients[2]
+  }
+  plot(by_date_dates[-c(1:6)], rates, type = "l", bty = "n", lwd = 3, xlab = "2022", ylab = "Doubling time (new daily cases)")
 }
